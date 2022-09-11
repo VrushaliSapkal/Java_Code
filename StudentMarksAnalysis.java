cc9f3a75db3b823bfd7eb74c0f6e579f42f9d07e
VrushaliSapkal <105592168+VrushaliSapkal@users.noreply.github.com>
2022-09-11 19:46:43 +0530

    Add files via upload

---
+++ StudentMarksAnalysis.java
@@ -0,0 +1,9 @@
+public class StudentMarksAnalysis {
+    public static void main(String[] args) {
+        int marks = 43;
+
+        String status = (marks >= 33) ? "Pass" : "Fail";
+        System.out.println(status);
+    }
+    
+}
